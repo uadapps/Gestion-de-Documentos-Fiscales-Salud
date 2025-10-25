70ff586943f2a5c0247975d3a602e41158f83ef0
SSFernando <fernandoderas22@gmail.com>
2025-10-25 10:32:56 -0600

    kjkj

---
+++ public/build/assets/spinner-4eNWRviO.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DN-UaI3C.js";import{c as i}from"./button-hq0xZ_1e.js";import{L as o}from"./loader-circle-Q0DZLHE1.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
